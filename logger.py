42800b36ae76c48a9936d263301c0b700dc3428a
disunln <150343931+disunln@users.noreply.github.com>
2026-03-23 11:59:31 -0700

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
